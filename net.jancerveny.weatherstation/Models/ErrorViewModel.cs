c4f0ec5fe36d0527ca5c9fa006be981feb73c50f
ohmy-cz <jcy@siteimprove.com>
2020-02-19 17:33:09 +0100

    Initial commit

---
+++ net.jancerveny.weatherstation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace net.jancerveny.weatherstation.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
